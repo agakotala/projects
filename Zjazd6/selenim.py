339a60f2c506ad5e66458e46910e67a41a32d30b
agakotala <agnieszka.szubryt09@gmail.com>
2025-04-10 20:31:51 +0200

    Update selenim.py

--- Zjazd6/selenim.py
+++ Zjazd6/selenim.py
@@ -40,28 +40,3 @@ model = SVC(kernel='rbf')  #tylko radial base function, więcej wymiarów
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
-
-from selenium import webdriver
-from selenium.webdriver import Keys
-from time import sleep
-
-okno1_chrome = webdriver.Chrome()
-okno2_chrome = webdriver.Chrome()
-
-okno1_chrome.get('https://www.google.com/')
-okno2_chrome.get('https://allegro.pl')
-
-sleep(3)
-okno1_chrome.find_element('id','L2AGLb').click()
-sleep(3)
-
-search_field = okno1_chrome.find_element('name','q')
-search_field.clear()
-search_field.send_keys('Czy chat GPT opanuje świat?')
-search_field.send_keys(Keys.ENTER)
-
-sleep(3)
-
-
-okno1_chrome.close()
-okno2_chrome.close()
