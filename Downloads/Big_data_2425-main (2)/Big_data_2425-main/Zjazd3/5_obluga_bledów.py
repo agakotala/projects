89b57aff1dc48fd795728abdb4b5c99384b6ba82
agakotala <agnieszka.szubryt09@gmail.com>
2025-04-11 20:15:21 +0200

    Delete Downloads/Big_data_2425-main (2) directory

--- Downloads/Big_data_2425-main (2)/Big_data_2425-main/Zjazd3/5_obluga_bledów.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        a = int(input('Podaj dzielną:   '))
-        b = int(input('Podaj dzielnik:   '))
-        break
-    except:
-        print('Zła wartośc, jeszcze raz')
-
-print('Dalej')
--- Downloads/Big_data_2425-main (2)/Big_data_2425-main/Zjazd3/8_obsluga_bledow.py
+++
@@ -1,5 +0,0 @@
-age = int(input('Ile masz lat?  '))
-if age <= 0:
-    raise ValueError('Wiek musi byc dodatni')
-
-print('Done')
