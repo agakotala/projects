c66b9460660db1970eaa0a1a3aeb61a1c79454fb
agakotala <agnieszka.szubryt09@gmail.com>
2025-04-11 20:15:03 +0200

    Delete Downloads/Big_data_2425-main (2)/Big_data_2425-main/Zjazd1 directory

--- Downloads/Big_data_2425-main (2)/Big_data_2425-main/Zjazd1/8_operacje_na_stringach.py
+++
@@ -1,9 +0,0 @@
-slowo = 'mama'
-napis = 'PieS piesek pieseczek'
-
-print(slowo.replace('a', 'A', 1))
-nowy_napis = napis.split()
-print('napis ma', len(nowy_napis), 'słów')
-print(napis.lower())
-
-
